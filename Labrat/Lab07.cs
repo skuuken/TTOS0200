50714d187b287f6bee6b49157cb2a481c63ebddd
Saku Hopponen <h8801@student.jamk.fi>
2017-10-25 10:50:12 +0300

    Lab07 added

---
+++ Labrat/Lab07.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labrat
+{
+    class Lab07
+    {
+        public static void Tehtava1()
+        {
+            //KÄYTTÄKÄÄ AINA TRY-CATCHIA
+            try
+            {
+                string nimi;
+                string filupath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string filu = filupath + @"\testi.txt";
+                //kirjoitetaan käyttäjän antamat rivit tiedostoon
+                //luodaan StreamWriter tyyppinen olio johon kirjoitetaan
+                StreamWriter sw = new StreamWriter(filu);
+                do
+                {
+                    Console.Write("Anna nimi (Enter lopettaa)");
+                    nimi = Console.ReadLine();
+                    if (nimi.Length > 0)
+                    {
+                        sw.WriteLine(nimi);
+                    }
+                } while (nimi.Length != 0);
+                sw.Close();
+                //avataan tiedosto uudestaan ja luetaan sen sisältö ja näytetään konsolissa
+                if (File.Exists(filu))
+                {
+                    string teksti = File.ReadAllText(filu);
+                    Console.WriteLine(teksti);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
--- Labrat/Program.cs
+++ Labrat/Program.cs
@@ -20,7 +20,7 @@ namespace Labrat
     {
         static void Main(String[] args)
         {
-            Lab06.TestaaPakka();
+            Lab07.Tehtava1();
         }
     }
 }
